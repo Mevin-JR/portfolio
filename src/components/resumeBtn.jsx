4fc64cfdd3a84ed4ea094ec1b79a58009463f114
Mevin-JR <supposedlysamyt@gmail.com>
2025-06-21 18:32:36 +0530

    Responsive resume preview

--- src/components/resumeBtn.jsx
+++ src/components/resumeBtn.jsx
@@ -4,8 +4,10 @@ import { pdfjs, Document, Page } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
+import { motion, AnimatePresence } from "motion/react";
+
 import { Download, Minus, Plus, RotateCcw, View } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Tooltip from "./tooltip";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
@@ -15,20 +17,58 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 
 export default function ResumeBtn() {
   const [showResume, setShowResume] = useState(false);
+  const [resumeScaleConst, setResumeScaleConst] = useState(1);
   const [resumeScale, setResumeScale] = useState(1);
+  const [maxScale, setMaxScale] = useState(1.4);
+  const [minScale, setMinScale] = useState(0.8);
 
-  const toggleShowResume = () => setShowResume((prev) => !prev);
+  // Smaller Devices (768px)
+  const SCALE_SM = 0.5;
+  const MIN_SCALE_SM = 0.3;
+  const MAX_SCALE_SM = 0.9;
+
+  // Medium Devices (1024px)
+  const SCALE_MD = 0.8;
+  const MIN_SCALE_MD = 0.4;
+  const MAX_SCALE_MD = 1.2;
+
+  useEffect(() => {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth < 768) {
+      setResumeScaleConst(SCALE_SM);
+
+      setResumeScale(SCALE_SM);
+      setMinScale(MIN_SCALE_SM);
+      setMaxScale(MAX_SCALE_SM);
+    } else if (screenWidth < 1024) {
+      setResumeScaleConst(SCALE_MD);
+
+      setResumeScale(SCALE_MD);
+      setMinScale(MIN_SCALE_MD);
+      setMaxScale(MAX_SCALE_MD);
+    }
+  }, []);
+
+  const toggleShowResume = () => {
+    setShowResume((prev) => !prev);
+    resetResumeScale();
+  };
 
   const reduceResumeScale = () => {
-    if (resumeScale > 0.8) {
-      setResumeScale((prev) => prev - 0.1);
+    if (resumeScale > minScale) {
+      setResumeScale((prev) => prev - 0.2);
     }
   };
 
   const increaseResumeScale = () => {
-    if (resumeScale < 1.5) {
-      setResumeScale((prev) => prev + 0.1);
+    if (resumeScale < maxScale) {
+      setResumeScale((prev) => prev + 0.2);
     }
+  };
+
+  const resetResumeScale = () => {
+    setResumeScale(resumeScaleConst);
   };
 
   return (
@@ -44,62 +84,82 @@ export default function ResumeBtn() {
         </button>
         <div className="absolute z-0 inset-0 bg-white rounded scale-x-0 origin-left transition-all duration-300 group-hover:scale-x-100"></div>
       </div>
-      {showResume && (
-        <div
-          className="absolute z-30 top-0 left-0 flex items-center justify-center w-screen h-screen bg-black/55"
-          onClick={() => setShowResume(false)}
-        >
-          <div
-            id="resume-preview"
-            className="relative p-10 bg-gray-900/80 rounded max-w-2xl max-h-screen overflow-auto"
-            style={{ userSelect: "text" }}
-            onClick={(e) => e.stopPropagation()}
+      <AnimatePresence>
+        {showResume && (
+          <motion.div
+            className="absolute z-30 top-0 left-0 flex items-center justify-center w-screen h-screen bg-black/55"
+            onClick={() => setShowResume(false)}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
           >
-            <div className="fixed z-10 bottom-24 left-1/2 transform -translate-x-1/2 flex gap-1">
-              <ul className="flex items-center justify-center bg-gray-900 rounded-2xl">
-                <Tooltip text="Zoom In">
-                  <li
-                    className="flex items-center justify-center rounded-r-none w-full h-full p-2 cursor-pointer transition-colors duration-300 hover:bg-white/25"
-                    onClick={increaseResumeScale}
-                  >
-                    <Plus />
-                  </li>
+            <motion.div
+              id="resume-preview"
+              className="relative p-10 rounded max-w-2xl max-h-screen overflow-auto"
+              style={{ userSelect: "text" }}
+              onClick={(e) => e.stopPropagation()}
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0 }}
+            >
+              <Document file="/Mevin_JR_Resume.pdf">
+                <Page pageNumber={1} scale={resumeScale} />
+              </Document>
+              <motion.div
+                className="fixed z-10 bottom-[110px] left-1/2 flex gap-1"
+                initial={{ y: "15%", opacity: 0, x: "-50%" }}
+                animate={{ y: 0, opacity: 1, x: "-50%" }}
+                exit={{ y: "15%", opacity: 0, x: "-50%" }}
+                transition={{
+                  type: "spring",
+                  stiffness: 400,
+                  damping: 20,
+                  delay: 0.25,
+                }}
+              >
+                <ul className="flex items-center justify-center bg-gray-900 rounded-2xl">
+                  <Tooltip text="Zoom In">
+                    <li
+                      className="flex items-center justify-center rounded-r-none w-full h-full p-2 cursor-pointer transition-colors duration-300 hover:bg-white/25"
+                      onClick={increaseResumeScale}
+                    >
+                      <Plus />
+                    </li>
+                  </Tooltip>
+                  <Tooltip text="Zoom out">
+                    <li
+                      className="flex items-center justify-center w-full h-full p-2 cursor-pointer transition-colors duration-300 hover:bg-white/25"
+                      onClick={reduceResumeScale}
+                    >
+                      <Minus />
+                    </li>
+                  </Tooltip>
+                  <Tooltip text="Reset">
+                    <li
+                      className="flex items-center justify-center rounded-l-none w-full h-full px-3 py-2 cursor-pointer transition-colors duration-300 hover:bg-white/25"
+                      onClick={resetResumeScale}
+                    >
+                      <RotateCcw size={20} />
+                    </li>
+                  </Tooltip>
+                </ul>
+                <Tooltip text="Download">
+                  <div className="z-10 bg-gray-900 rounded-2xl">
+                    <a
+                      className="flex items-center justify-center w-full h-full p-3 cursor-pointer transition-colors duration-300 hover:bg-white/25"
+                      href="/Mevin_JR_Resume.pdf"
+                      download
+                    >
+                      <Download />
+                    </a>
+                  </div>
                 </Tooltip>
-                <Tooltip text="Zoom out">
-                  <li
-                    className="flex items-center justify-center w-full h-full p-2 cursor-pointer transition-colors duration-300 hover:bg-white/25"
-                    onClick={reduceResumeScale}
-                  >
-                    <Minus />
-                  </li>
-                </Tooltip>
-                <Tooltip text="Reset">
-                  <li
-                    className="flex items-center justify-center rounded-l-none w-full h-full px-3 py-2 cursor-pointer transition-colors duration-300 hover:bg-white/25"
-                    onClick={() => setResumeScale(1)}
-                  >
-                    <RotateCcw size={20} />
-                  </li>
-                </Tooltip>
-              </ul>
-              <Tooltip text="Download">
-                <div className="z-10 bg-gray-900 rounded-2xl">
-                  <a
-                    className="flex items-center justify-center w-full h-full p-3 cursor-pointer transition-colors duration-300 hover:bg-white/25"
-                    href="/Mevin_JR_Resume.pdf"
-                    download
-                  >
-                    <Download />
-                  </a>
-                </div>
-              </Tooltip>
-            </div>
-            <Document file="/Mevin_JR_Resume.pdf">
-              <Page pageNumber={1} scale={resumeScale} />
-            </Document>
-          </div>
-        </div>
-      )}
+              </motion.div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
